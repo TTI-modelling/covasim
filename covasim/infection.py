010f54a1743f3aa4403520d6546d13320b0c722d
Martyn <martyn1fyles@gmail.com>
2021-03-26 15:03:08 +0000

    Implement default infection dynamics class

--- covasim/infection.py
+++ covasim/infection.py
@@ -1,6 +1,10 @@
 '''
 Specify a class that allows users to apply custom infection dynamics
 '''
+from . import defaults as cvd
+from . import utils as cvu
+
+
 class InfectionDynamics():
 
     '''
@@ -26,6 +30,8 @@ class InfectionDynamics():
         '''
         Takes the simulation object as an input and compute the relative transmissibility for infectious nodes.
 
+        Usually gets called every time step
+
         Args:
             sim: The Sim instance
 
@@ -33,3 +39,21 @@ class InfectionDynamics():
             None
         '''
         raise NotImplementedError
+
+class HighLowInfectiousness(InfectionDynamics):
+
+    def compute_relative_transmissibility(self, sim):
+        '''
+        Computes relative transmissibility using a default implementation of infectiousness
+        '''
+        
+        # unpack relavent parameters from this step of the simulation
+        frac_time    = cvd.default_float(sim['viral_dist']['frac_time'])
+        load_ratio   = cvd.default_float(sim['viral_dist']['load_ratio'])
+        high_cap     = cvd.default_float(sim['viral_dist']['high_cap'])
+        date_inf     = sim.people.date_infectious
+        date_rec     = sim.people.date_recovered
+        date_dead    = sim.people.date_dead
+        t            = sim.ts
+
+        return cvu.high_low_transmissibility(t, date_inf, date_rec, date_dead, frac_time, load_ratio, high_cap)
